ec8b0638a7a9bc804f7e598e8362476986acb1e6
Choi-comsa451 <66867676+Choi-comsa451@users.noreply.github.com>
2020-06-13 18:39:15 +0900

    생활코딩 web과정의 html 들으면서 연습한 내용들임

---
+++ 001.js
@@ -0,0 +1,10 @@
+/*function choiManner001() {
+  console.log("choi~")
+}*/
+
+//const title = document.getElementById("#001_title")
+//const title = document.querySelector("#title")
+//console.log(title);
+//title.style.color= "yellow";
+//console.error("fuck");
+//title.innerHTML = "hi from JS"
